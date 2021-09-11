269a5522eed14e6565a5d5a03dfe1a5291e7822e
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2021-09-11 07:07:20 +0530

    Update varData.js

--- js/varData.js
+++ js/varData.js
@@ -30,9 +30,9 @@ var data={
 		student:{},
 		form:{
 			options:[
-				"4","3","2","1","<BR>","<UL>","<LI>","<H1>","<LI>","<UL>","</A>","<OL>","<LI>","<STYLE>","<P>","<HEAD>","<STYLE>","<CENTER>","<P>","<B>","<Table>","<Select>","<Data>","None of These","वेब पेज बनाने के लिए","लिंक दिखने के लिए","हाइपरलिंक ऐड करने के लिए","इनमे से कोई नहीं","<B>","<A>","<BR>","<P>","<H2>","<H3>","<H1>","<H4>","Ctrl","Shift","Alt","Window","HTML file","all file","doc file","1 & 2","Internet use","Web page making","a & b both","None of these","<body>","<p>","<head>","<br>","<br>","<pre>","<a>","<b>","*","*","*","*","src","href","lang","None of these","text के रंग को सेट करने में","Background का रंग सेट करने में","a और b दोनों","उपरोक्त में से कोई नहीं","background से वेब पेज में बैकग्राउंड इमेज (फोटो) लगा सकते है","background से वेब पेज में बैकग्राउंड इमेज (फोटो) लगा सकते है","background से वेब पेज में बैकग्राउंड इमेज (फोटो) लगा सकते है","background से वेब पेज में बैकग्राउंड इमेज (फोटो) लगा सकते है","Paragraph tag","Print tag","Protocol tag","None of these","src","style","lang","href","<label> tag in HTML is used for creating a tag for form elements.","<label> can be used to increase the clickable area of buttons","id attribute is used with to increase the clickable area of form elements","None of these","<select>","<text>","<textarea>","<dropdown>","Empty tags","Container tags","1 & 2 both","None of these","गूगल","माइक्रोसॉफ्ट","फेसबुक","वर्ल्ड वाइड वेब संघ","<h6>","<h1>","<head>","heading>"
+				"Scratch","Tempo","Sprite","All Of The Above","Text","Numeric","Picture","Video","Monitor","Projector","End User","Painter","Delete","Enter","backspace","space","Circle","Square","Rectangle","Oval","Variable में।","Program में।","Memory में।","None of these","ELSE","THEN","ELSIF","WHEN","Concurrent","Sequential","Assignment","Selected Assignment","Sound","Images","Animation","Video","Beats per second","Beats per minute","bytes per second","bits per second"
 			],
-			action:"https://docs.google.com/forms/d/e/1FAIpQLSc1Ram88noReb5peY2TZRr2qrY8oJODJoGsYbTb-zSiIb066Q/formResponse",
+			action:"https://docs.google.com/forms/d/e/1FAIpQLSc-r4b8X8az7_-8YXPWMSRU3-7DAmmMJxHX1WgCm6vGn_PQvw/formResponse",
 			defaultTags:()=>{
 				return `
 				<input name="entry.1794899332" value="BCCL-JH-ICT2-PAL-094 [ RAJKIYEKRIT SURAT HIGH SCHOOL LOHRA ]">
@@ -45,10 +45,12 @@ var data={
 				<input name="entry.919860739" value="${gender}">
 
 				<input name="pageHistory" value="0,1,2">
-				<input name="fbzx" value="-8890258034405686936">
+				<input name="fbzx" value="8206625321620636990">
 				`
 			},
-			questionList:["entry.834768297","entry.315835193","entry.278845054","entry.1654528168","entry.50318473","entry.1397102095","entry.954704599","entry.460350148","entry.1150164949","entry.1030204631","entry.2059653833","entry.96074418","entry.471768160","entry.1877356267","entry.1061914541","entry.1898206548","entry.456667498","entry.82873413","entry.2093634869","entry.698301773","entry.1024016824","entry.1832350817","entry.597901340","entry.1416220811","entry.203599965"]
+			questionList:[
+			"entry.2143319819","entry.577919629","entry.910946363","entry.1290390412","entry.1753374106","entry.1082109704","entry.281177373","entry.1948237999","entry.275918241","entry.365912161"
+			]
 		}
 	},
 	11:{
@@ -63,9 +65,9 @@ var data={
 
 		form:{
 			options:[
-				"Software","Hardware","User","None of these","Create document","Format document","Adjust the layout","All of these","Word Star","Word Star","Word Star","Word Star","Excel application","drawing application","Word application","Tally application",".doc",".txt",".ppt",".xls","Line Delete","Line Delete","Line Delete","Line Delete","4","2","1","3","Edit","File","View Menu","Chek Box","Operating system","Application Software","1 & 2 Both","None of these","Ctrl + X","Ctrl + V","1 & 2 Both","Ctrl + A","Software","Operating System","Folder","File","Copy","Rectangular Selection","Freeform Selection","Rectangular Selection & Freeform Selection दोनों","Ribbon","Toolbar","Drawingbar","File","Open","Select","All program","Paint","Start, all programs, accessories, paint","All programs, accessories, paint, start","Paint, all programs, accessories, start","All programs, start, accessories, paint","कलर पैलेट","मेनू बार","स्टेटस बार","उपरोक्त में से कोई नहीं","Noteped","Ms Paint","Calculator","उपरोक्त में से कोई नहीं","पेंसिल","फील विथ कलर","टेक्सट","इरेज़र","Icon","Delete Button","My Computer","Start Button","Right mouse button","Left mouse button","Scroll Wheel","इनमें से कोई नहीं","Ctrl+C","Ctrl+V","Ctrl+K","Ctrl+N","Page Setup","Page Orientation","Page Layout","Page Margin","Page Setup Dialog Box","Standard Toolbar","Formatting Toolbar","Header & Footer Dialog Box","मार्जिन, पेपर साइज, कलर","पेपर स्‍त्रोत, ले आउट","उपरोक्त दोनों","उपरोक्त में से कोई नहीं","फ़ाइल का रूप","फ़ाइल का नाम","फ़ाइल का प्रकार","उपरोक्त में से कोई नहीं"
+				"Messaging","Copy","Excel","ppt","Electronic","Election","Electric","Electricity","Audio","Video","Images","उपरोक्त सभी","मोबाइल","बैंक","स्कूल या कॉलेज","सभी में","E-mail","I-Mail","Gmail","इनमे से कोई नहीं","Yahoo Mail","Gmail","iCloud mail","NetEase Mail","GOOGLE","APPLE","YAHOO","इनमे से कोई नहीं","PowerPoint","Inbox","Spam","Draft","Block Carbon Copy","Blind Carbon Copy","Blank Copy","इनमे से कोई नहीं","Mailbox","Subject","URL","इनमे से कोई नहीं"
 			],
-			action:"https://docs.google.com/forms/d/e/1FAIpQLScwbtuEX2sO5pI1aFxnkernXFYxzAU7ZIDT9bCmjf-iSrIDVA/formResponse",
+			action:"https://docs.google.com/forms/d/e/1FAIpQLSfOZe2IZCZ4Y0xkNYgk3W3OMLXjWjvg0gIF9UZNMSBNRADl9Q/formResponse",
 			defaultTags:()=>{
 				return `
 				<input name="entry.1270814050" value="BCCL-JH-ICT2-PAL-094 [ RAJKIYEKRIT SURAT HIGH SCHOOL LOHRA ]">
@@ -78,9 +80,11 @@ var data={
 				<input name="entry.919860739" value="${gender}">
 
 				<input name="pageHistory" value="0,1,2">
-				<input name="fbzx" value="2055812261201590731">`
+				<input name="fbzx" value="3375528895304106637">`
 			},
-			questionList:["entry.1895883406","entry.450355357","entry.1284904058","entry.916679564","entry.238629542","entry.682986720","entry.1161368412","entry.1520078659","entry.1057522759","entry.1758772272","entry.1215386439","entry.1943758776","entry.788872138","entry.1193620013","entry.1831409065","entry.477652656","entry.229130841","entry.1166684027","entry.329261897","entry.891680793","entry.1710125064","entry.1500945263","entry.1781252479","entry.1999802559","entry.1374043504"]
+			questionList:[
+			"entry.1019191142","entry.1617823604","entry.1020284696","entry.362208212","entry.755309480","entry.394302259","entry.807886439","entry.1654589482","entry.798110205","entry.1504311434"
+			]
 		}
 	},
 
@@ -96,9 +100,9 @@ var data={
 
 		form:{
 			options:[
-				"डेटा स्टोर करने","डेट व्यवस्थित करने","उपरोक्त दोनों","इनमें से की नहीं","cell","column","row","none of these","count, sum, average, percentage etc","chart, graph","Data Analysis","उपरोक्त सभी","To find the largest value","To find the Smallest value","To find the mid value","None of these","एम एस एक्सेल","गूगल शीट","लिब्रेऑफिस कैल्क","एम एस वर्ड","A1","1A","*A","=A","Insert Cell","Delete Cell","Format Celll","इनमे से कोई नहीं","Horizontally","Vertical","Freeze","उपरोक्त सभी","Ctrl+ F","Alt+ F","Ctrl+ W","Alt+ W","=sum","sum =","=sum(address1:address2)","इनमे से कोई नहीं","Phishing","Bulling","Stalking","Identity thef","Malware","Spyware","Adware","All of the above","Antivirus","Firewall","Cookies","Malware","Piracy","Plagiarism","Intellectual property rights","All of the above","Information Tech Act -2000","Indian Technology Act -2000","International Technology Act -2000","Information Technology Act -2000","पासवर्ड अटैक","फिशिंग","मेल वायर इंजिग","डिनायल ऑफ़ सर्विसेज","जिन वेबसाइट के एड्रेस की शुरुआत में https:// नहीं होता है","जिन वेबसाइट में पेडालॉक नहीं होता है","जिन वेबसाइट में पेडालॉक नहीं होता है व जिन वेबसाइट के एड्रेस की शुरुआत में https:// नहीं होता है दोनों","उपरोक्त में से कोई नहीं","पासवर्ड अटेक","डिनायल ऑफ़ सर्विसेज","फिशिंग","उपरोक्त सभी","आय कर","औद्यागिक प्रौद्योगिकी","सूचना प्रौद्योगिकी","इनसाइडर ट्रेडिंग","उसे अपने ईमेल प्रदाता के माध्यम से फ़िशिंग/स्पैम के रूप में रिपोर्ट करेंगे","कुछ नहीं करेंगे","संदेश को अपने ईमेल और पासवर्ड के सथा उत्तर देंगे।","इनसे में कोई भी नहीं","International Organization for Standardization","International Organization for Security","International Standardization for Operations","Indian Standard Organization","कॉपीराइट","हैकिंग","विश्वासघात","साइबर धोखाधड़ी","ISO2700","ISO27001","ISO27002","ISO27003","स्पाईवेयर","टोजनहॉर्स","वायरस","उपरोक्त सभी","कंप्यूटर इनपुट प्रदान करने में बाधाएं","आउटपुट की व्याख्या करना","सहायक दस्तावेज पढ़ना","उपरोक्त में से सभी"
+				"File","Insert","View","Page Layout","ctrl+F2","ctrl+P","ctrl+S","ctrl+X","Drop-down arrows appear for the header cell in each column.","A filter dialogue box will open with options.","You will see a check list of all the column titles.","Nothing happens.","Sort Data","Data Validation","Advanced Filter","Remove Duplicates","ctrl+F2","ctrl+P","Ctrl +Shift+O","ctrl+X","एक्सेल शीट (Excel Sheet)","वर्कशीट (Worksheet)","एक्सेल वर्कशीट (Excel Worksheet)","वर्कबुक (Work Book)","LibreOffice Calc","LibreOffice Impress","Google Sheet","iWork Numbers","Descending","Largest","Ascending","Smallest","Text to Table","Text to Column","Convert","None of these","क्रमबद्ध रखना","कहीं भी रख देना","1 तथा 2 दोनों","इनमें से कोई नहीं"
 			],
-			action:"https://docs.google.com/forms/d/e/1FAIpQLSeXPwnpYobqQuBu_QfJVISmROLjFi6fCofYHJHGI-l175iYBA/formResponse",
+			action:"https://docs.google.com/forms/d/e/1FAIpQLScUJdmPuSDY3oz2bCFWOPcmtGRRweM9OiAZ2Cq07SA5xS3JEw/formResponse",
 			defaultTags:()=>{
 				return `
 				<input name="entry.658528083" value="BCCL-JH-ICT2-PAL-094 [ RAJKIYEKRIT SURAT HIGH SCHOOL LOHRA ]">
@@ -111,9 +115,11 @@ var data={
 				<input name="entry.919860739" value="${gender}">
 
 				<input name="pageHistory" value="0,1,2">
-				<input name="fbzx" value="3869625210270808250">`
+				<input name="fbzx" value="-9086784847810382128">`
 			},
-			questionList:["entry.2141135245","entry.742849946","entry.2071153655","entry.1263948529","entry.1542441521","entry.1064001019","entry.2132859938","entry.103281991","entry.97743496","entry.1303307375","entry.600251233","entry.501567248","entry.2117660976","entry.1212645235","entry.750161773","entry.1026268247","entry.1499154056","entry.559762458","entry.1066893185","entry.1172098696","entry.1232192277","entry.766694947","entry.1620278293","entry.749169241","entry.1842052344"]
+			questionList:[
+			"entry.1703989583","entry.95199612","entry.573589050","entry.1742450868","entry.89122004","entry.2014408939","entry.368016924","entry.963551864","entry.1359435364","entry.751947841"
+			]
 		}
 	},
 
